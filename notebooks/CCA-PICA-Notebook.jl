b7e7b729feb3c54cdccb128ad073122fc2af7860
Jeffrey Varner <jdv27@cornell.edu>
2022-06-21 12:31:38 -0400

    structural documents
    structure analysis documents

--- notebooks/CCA-PICA-Notebook.jl
+++ notebooks/CCA-PICA-Notebook.jl
@@ -12,6 +12,7 @@ begin
 	using CSV
 	using LinearAlgebra
 	using Statistics
+	using DelimitedFiles
 end
 
 # ╔═╡ 41d68deb-3426-4586-a085-7e6fe19be8b4
@@ -22,8 +23,10 @@ Jeffrey D Varner, Professor, Robert Frederick Smith School of Chemical and Biomo
 
 # ╔═╡ 1fd0757b-537b-4e61-bebf-d7da7f00d2ed
 md"""
-##### Modeling the relationship between courses and performance indicators
-The course mapping matrix $M$ related the courses taught in the core curriculum of the Smith School to the performance indicators (PIs) developed by the College of Engineering Educational Assessment Committee (COEEAC). The mapping matrix $M$ was constructed using the instructor-generated self-assessments for each CBE course. The course mapping matrix $M$ is a $\mathcal{C}\times\mathcal{P}$ binary matrix, where $\mathcal{C}$ denotes the number of courses. In contrast, $\mathcal{P} $ denotes the number of performance indicators used for assessment. The $(i,j)$ value of $M$, denoted by $m_{ij}$ can take on two values, $0$ or $1$:
+#### Modeling the relationship between courses and performance indicators
+The course mapping matrix $M$ related the courses taught in the core curriculum to the performance indicators (PIs) developed by the College of Engineering Educational Assessment Committee (COEEAC). The mapping matrix $M$ was constructed using the instructor-generated self-assessments for each CBE course. 
+
+The course mapping matrix $M$ is a $\mathcal{C}\times\mathcal{P}$ binary matrix, where $\mathcal{C}$ denotes the number of courses and $\mathcal{P} $ denotes the number of performance indicators used for assessment. The $(i,j)$-th value of $M$, denoted by $m_{ij}$ takes on the values:
 
 $$m_{ij} = 
 \begin{cases}
@@ -31,20 +34,22 @@ $$m_{ij} =
 	0 & \text{if course i not assessed using performance indicator j}
 \end{cases}$$
 
-Thus, the rows of the matrix $M$ correspond to the CBE courses assessed in this cycle $\mathcal{C} = 12$, while the columns correspond to COEEAC performance indicators $\mathcal{P} = 17$. Operations on the matrix $M$ can give structural information about the CBE program and how it is assessed. 
+Thus, the rows of the matrix $M$ correspond to the CBE courses assessed (in this cycle $\mathcal{C} = 12$), while the columns correspond to COEEAC performance indicators ($\mathcal{P} = 17$). Operations on the matrix $M$ give structural information about the CBE program and how it is assessed. 
 
-__Course connectivity array (CCA)__: The product of the matrix $M$ with its transpose (denoted by $\star^{T}$) gives the $\mathcal{C}\times\mathcal{C}$ course connectivity array (CCA):
+##### Course connectivity array
+The product of the matrix $M$ with its transpose (denoted by $\star^{T}$) gives the $\mathcal{C}\times\mathcal{C}$ course connectivity array (CCA):
 
-$$CCA \equiv MM^{T}$$
+$$\text{ CCA } \equiv MM^{T}$$
 
-The diagonal entries of $CCA$ describe the number of PIs used to assess that course. In contrast, off-diagonal elements of $CCA$ describe the number of shared PIs between classes $i$ and $j$, where $i\neq{j}$. Thus, $CCA$ helps determine the assessment coverage for a particular course and the assessment relationship between classes.
+The diagonal entries of the CCA describe the number of PIs used to assess a course. In contrast, the off-diagonal elements of CCA describe the number of shared PIs between classes $i$ and $j$, where $i\neq{j}$. Thus, the CCA measures the assessment coverage for a particular course and the assessment relationship between classes.
 
 
-__Performance Indicator connectivity array (PICA)__: The product of the transpose of the mapping matrix $M^{T}$ with its itself:
+##### Performance indicator connectivity array 
+The product of the transpose of the mapping matrix $M^{T}$ with its itself:
 
-$$PICA \equiv M^{T}M$$
+$$\text{ PICA } \equiv M^{T}M$$
 
-gives the $\mathcal{P}\times\mathcal{P}$ performance indicator connectivity array (PICA). The diagonal entries of $PICA$ describe the number of courses that used a particular PI. On the other hand, the off-diagonal elements of $PICA$ represent the number of shared classes between performance indicator $i$ and $j$, where $i\neq{j}$.
+gives the $\mathcal{P}\times\mathcal{P}$ performance indicator connectivity array (PICA). The diagonal entries of PICA describe the number of courses that used a particular PI. On the other hand, the off-diagonal elements of PICA describe the number of shared classes between performance indicator $i$ and $j$, where $i\neq{j}$. Thus, PICA measures which performance indicators are being used by instructors to measure student achievement. 
 
 """
 
@@ -56,7 +61,8 @@ begin
 	
 	# load the data as a DataFrame -
 	df = CSV.read(path_to_data_file,DataFrame)
-
+	
+	
 	# create the mapping matrix M -
 	M = Matrix(df[!,5:end])
 
@@ -65,90 +71,27 @@ begin
 end
 
 # ╔═╡ 41de88b3-c1d4-494b-aa53-cff07e4b58c1
-# compute the course connectivity array -
-CCA = M*transpose(M)
+begin
+	# compute the course connectivity array -
+	CCA = M*transpose(M)
+
+	# uncomment me to save to disk -
+	writedlm(joinpath(pwd(),"data","CCA.csv"),CCA, ',')
+
+	CCA
+end
 
 # ╔═╡ 98830429-e731-4915-9a3e-3f1640d810c4
-# compute the PI connectivity array (PICA) -
-PICA = transpose(M)*M
-
-# ╔═╡ 10f76d44-1473-40bd-ad94-97c3ba0b5668
-md"""
-##### Modeling the relationship between courses and student outcomes
-The mapping matrix $M$ relates $\mathcal{C}$ courses to $\mathcal{P}$ performance indicators. However, to explore the relationship between classes and program educational outcomes (PEOs), we need to map the $\mathcal{P}$ performance indicators to the $\mathcal{S}$ student outcomes. The College of Engineering Educational Assessment Committee (COEEAC) already developed these relationships, which we encode in the $\mathcal{S}\times{P}$ matrix $L$: 
-
-$$l_{ij} = 
-\begin{cases}
-	\alpha & \text{if student outcome i is connected with performance indicator j} \\
-	0 & \text{if student outcome i is connected with performance indicator j}
-\end{cases}$$
-
-where $\alpha$ denotes the fraction of outcome $i$ represented by performance indicator $j$. We assume that each performance indicator contributes equally to each student outcome. Thus, if the set of performance indicators belonging to a particular student outcome has $n$ elements, then $\alpha = 1/n$,
-
-The $\mathcal{S}\times\mathcal{P}$ student outcomes matrix $S$ is given by:
-
-$$S = LM^{T}$$
-
-Connectivity analysis of the matrix $S$ gives us insights into share outcomes:
-* The $SS^{T}$ array with dimension $\mathcal{S}\times\mathcal{S}$ holds information about the number of outcomes per course (diagonal) and the shared outcomes between class $i$ and $j$, where $i\neq{j}$.
-* The $S^{T}S$ array with dimension $\mathcal{C}\times\mathcal{C}$  holds information about the number of courses per outcome (diagonal) and classes shared between outcomes $i$ and $j$, where $i\neq{j}$
-"""
-
-# ╔═╡ 7df2a516-9954-43bf-b5af-0d0c7c940270
 begin
+	# compute the PI connectivity array (PICA) -
+	PICA = transpose(M)*M
 
-	# path to binary PEO to SO array -
-	path_to_outcomes_PI_data_file = joinpath(pwd(),"data","Outcomes-PI-BinaryMapping-Array.csv")
+	# uncomment me to save to disk -
+	writedlm(joinpath(pwd(),"data","PICA.csv"),PICA, ',')
 
-	# load the data as a DataFrame -
-	df_so_pi = CSV.read(path_to_outcomes_PI_data_file, DataFrame)
-
-	# generate the PEO matrix P -
-	L = Matrix(df_so_pi[!,2:end])
-	
-	# show -
-	nothing 
+	# show PICA -
+	PICA
 end
-
-# ╔═╡ 70a3f9d0-6be4-42f4-89b6-daf45a3b0202
-S = L*transpose(M)
-
-# ╔═╡ 8b0642d8-5cff-440f-973e-5c28395a620b
-S*transpose(S)
-
-# ╔═╡ 76e6c96c-fca0-4e6c-8f78-d859f922f265
-md"""
-##### Modeling the relationship between student outcomes and program educational outcomes (PEO)
-"""
-
-# ╔═╡ 78c39051-c596-4c30-9b75-fa388157bc39
-begin
-
-	# path to binary PEO to SO array -
-	path_to_peo_data_file = joinpath(pwd(),"data","BinaryMapping-PEOs-to-SO.csv")
-
-	# load the data as a DataFrame -
-	df_peo = CSV.read(path_to_peo_data_file, DataFrame)
-
-	# generate the PEO matrix P -
-	P = Matrix(df_peo[!,2:end])
-	
-	# show -
-	nothing 
-end
-
-# ╔═╡ 4e0f0d59-4e91-47ff-8b74-ec544c246e9c
-T = P*S
-
-# ╔═╡ 8383bef6-8bcc-492c-8a6f-0eef1280b8e7
-begin	
-	R = sum(T,dims=2)
-	Z = sum(R)
-	Rₛ = (1/Z)*R
-end
-
-# ╔═╡ fc76a7e7-7ac4-4bae-8889-07cf846b82b7
-
 
 # ╔═╡ a45da81a-edb4-11ec-2f66-2b11954e62aa
 html"""
@@ -173,6 +116,7 @@ PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
 CSV = "336ed68f-0bac-5ca0-87d4-7b16caf5d00b"
 DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+DelimitedFiles = "8bb1440f-4735-579b-a4ab-409b98df4dab"
 LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
 Statistics = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 
@@ -494,19 +438,10 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╔═╡ Cell order:
 # ╟─41d68deb-3426-4586-a085-7e6fe19be8b4
 # ╠═2dd9b648-636b-4e56-989a-00c32f95eea9
-# ╠═1fd0757b-537b-4e61-bebf-d7da7f00d2ed
+# ╟─1fd0757b-537b-4e61-bebf-d7da7f00d2ed
 # ╠═f3263a91-6989-4fb8-9092-bf048fe0096e
 # ╠═41de88b3-c1d4-494b-aa53-cff07e4b58c1
 # ╠═98830429-e731-4915-9a3e-3f1640d810c4
-# ╠═10f76d44-1473-40bd-ad94-97c3ba0b5668
-# ╠═7df2a516-9954-43bf-b5af-0d0c7c940270
-# ╠═70a3f9d0-6be4-42f4-89b6-daf45a3b0202
-# ╠═8b0642d8-5cff-440f-973e-5c28395a620b
-# ╟─76e6c96c-fca0-4e6c-8f78-d859f922f265
-# ╠═78c39051-c596-4c30-9b75-fa388157bc39
-# ╠═4e0f0d59-4e91-47ff-8b74-ec544c246e9c
-# ╠═8383bef6-8bcc-492c-8a6f-0eef1280b8e7
-# ╠═fc76a7e7-7ac4-4bae-8889-07cf846b82b7
-# ╠═a45da81a-edb4-11ec-2f66-2b11954e62aa
+# ╟─a45da81a-edb4-11ec-2f66-2b11954e62aa
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
